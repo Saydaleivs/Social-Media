67792d666cf2b1f1584d0d4c6b448767cb521d5f
saydaleivs <saydaleiv@gmail.com>
2023-01-20 10:23:35 +0500

    second commit

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const winston = require('winston')
 
 module.exports = function () {
-  const mongoURI = 'mongodb://127.0.0.1/web-media'
+  const mongoURI =
+    'mongodb+srv://saydaleivs:Ss20051018@cluster0.fprelfe.mongodb.net/?retryWrites=true&w=majority'
 
   mongoose.set('strictQuery', false)
   mongoose
